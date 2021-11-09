2f50d934f9dc4b3e094face808572676c448c826
Giselle Chacon <gisecn@gmail.com>
2021-11-09 19:18:19 +0000

    Updated visualizer deltas - Added state management redux

--- src/components/OrderBook/index.tsx
+++ src/components/OrderBook/index.tsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import Spread from '../Spread';
-import PriceLevelRow from './PriceLevelRow';
-import { Container, TableContainer } from './styles';
-import TitleRow from './TitleRow';
+import React, { useEffect } from 'react';
+import TitleRow from "./TitleRow";
+import { Container, TableContainer } from "./styles";
+import PriceLevelRow from "./PriceLevelRow";
+import Spread from "../Spread";
+import { useAppDispatch, useAppSelector } from '../../state/Hooks/hooks';
+import { addAsks, addBids, addExistingState, selectAsks, selectBids } from './orderbookSlice';
 
 const WSS_FEED_URL: string = 'wss://www.cryptofacilities.com/ws/v1';
 const subscribeMessage = {
@@ -11,24 +13,17 @@ const subscribeMessage = {
   product_ids: ['PI_XBTUSD']
 };
 
-interface Delta {
+export interface Delta {
   feed: string;
   product_id: string;
   bids: [];
   asks: [];
-}
-
-interface ExistingState {
-  numLevels: number;
-  feed: string;
-  bids: [];
-  asks: [];
-  product_id: string;
 }
 
 const OrderBook = () => {
-  const [delta, setDelta] = useState<Delta>();
-  const [existingState, setExistingState] = useState<ExistingState>();
+  const bids = useAppSelector(selectBids);
+  const asks = useAppSelector(selectAsks);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     const ws = new WebSocket(WSS_FEED_URL);
@@ -39,9 +34,14 @@ const OrderBook = () => {
     ws.onmessage = (event) => {
       const response = JSON.parse(event.data);
       if (response.numLevels) {
-        setExistingState(response);
+        dispatch(addExistingState(response));
       } else {
-        // setDelta(response);
+        if (response?.bids?.length > 0) {
+          dispatch(addBids(response.bids));
+        }
+        if (response?.asks?.length > 0) {
+          dispatch(addAsks(response.asks));
+        }
       }
     };
     ws.onclose = () => {
@@ -51,7 +51,7 @@ const OrderBook = () => {
     return () => {
       ws.close();
     };
-  }, []);
+  }, [dispatch]);
 
   const formatNumber = (arg: number): string => {
     return new Intl.NumberFormat('en-US').format(arg);
@@ -61,15 +61,19 @@ const OrderBook = () => {
     return arg.toLocaleString("en", {useGrouping: true, minimumFractionDigits: 2})
   };
 
-  const buildPriceLevels = (levels: [], reversedOrder: boolean = false): React.ReactNode => {
+  const buildPriceLevels = (levels: number[][], reversedOrder: boolean = false): React.ReactNode => {
     const totalSums: number[] = [];
 
-    // Prepare data, i.e. add totals and depth
-    levels.map((level: any, idx) => {
-      level[2] = idx === 0 ? level[1] : level[1] + totalSums[idx - 1];
-      totalSums.push(level[2]);
-      return level;
-    });
+    // TODO: move out the calculation of the totals
+    // Add total amounts
+   /* const levelsWithTotals: number[][] = levels.map((level: number[], idx) => {
+      const size: number = level[1];
+      const updatedLevel = [...level];
+      const totalSum: number = idx === 0 ? size : size + totalSums[idx - 1];
+      updatedLevel[2] = totalSum;
+      totalSums.push(totalSum);
+      return updatedLevel;
+    });*/
 
     const maxTotal: number = Math.max.apply(Math, totalSums);
 
@@ -93,21 +97,23 @@ const OrderBook = () => {
 
   return (
     <Container>
-      {existingState ?
+      {bids && asks ?
         <>
           <TableContainer>
             <TitleRow />
-            {buildPriceLevels(existingState.bids)}
+            {buildPriceLevels(bids)}
           </TableContainer>
           <Spread />
           <TableContainer>
             <TitleRow reversedFieldsOrder={true} />
-            {buildPriceLevels(existingState.asks, true)}
+            {buildPriceLevels(asks, true)}
           </TableContainer>
         </> :
         <>No data.</>}
+
+
     </Container>
   )
 };
 
-export default OrderBook;
+export default React.memo(OrderBook);
